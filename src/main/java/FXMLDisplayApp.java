d0cdc2de7d5d1c195c8c78e1027dedd7cc17cf15
CyberFlareCoding <jggreeff22@gmail.com>
2023-10-04 19:58:55 -0400

    initial project

---
+++ src/main/java/FXMLDisplayApp.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class FXMLDisplayApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ftpmain.fxml")));
+
+        // Create a scene with the loaded FXML content
+        Scene scene = new Scene(root, 600, 400); // Set width and height as needed
+
+        // Set the scene on the primary stage
+        primaryStage.setScene(scene);
+
+        // Set the stage title
+        primaryStage.setTitle("FXML Display Example");
+
+        // Show the stage
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
