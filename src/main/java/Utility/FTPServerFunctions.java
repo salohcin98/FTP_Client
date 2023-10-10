5873a5fcc4c566a60271f48c9cd1c04f174dd79e
nicholasfranck <nfranck@email.uscupstate.edu>
2023-10-10 15:58:49 -0400

    class names

---
+++ src/main/java/Utility/FTPServerFunctions.java
@@ -0,0 +1,27 @@
+package Utility;
+
+import java.io.IOException;
+import java.sql.*;
+
+
+public class FTPServerFunctions {
+    public void getUserFiles(){
+
+    }
+
+    public void uploadFileInfo(){
+
+    }
+
+    public void getFileInfo(){
+
+    }
+
+    public void fileShare() {
+
+    }
+
+    public void deleteFile(){
+        
+    }
+}
