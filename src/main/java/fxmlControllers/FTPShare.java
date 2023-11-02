196e347e2a64ccdcf36f25738809f6bdbb8874db
Eric Peasley <peasley@erics-air.attlocal.net>
2023-11-02 12:21:28 -0400

    comments

--- src/main/java/fxmlControllers/FTPShare.java
+++ src/main/java/fxmlControllers/FTPShare.java
@@ -1,34 +1,18 @@
 package fxmlControllers;
 
-import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+
 import javafx.fxml.FXML;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
-import java.io.File;
-
-public class FTPShare {
+public class FTPShare
+{
     @FXML
-    private Stage primaryStage;
-
-    private File selectedFile;
-
+    private Button shareButton;
     @FXML
-    private void browseFile(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Choose a File to Share");
-        selectedFile = fileChooser.showOpenDialog(primaryStage);
-    }
-
+    private TreeTableView userTable;
     @FXML
-    private void shareFile(ActionEvent event) {
-        if (selectedFile != null) {
-
-            System.out.println("Sharing file: " + selectedFile.getName());
-        }
-    }
-
-    public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-    }
+    private TreeTableColumn userList;
 }
